69a52e55bec5dce5f06579c69aed7764133afaa9
AndreyAD20 <anmdretyanso12@gmail.com>
2026-02-12 12:59:42 +0000

    eliminacion de archivos sin importancia

--- drop_telefono_column.sql
+++
@@ -1,8 +0,0 @@
--- Eliminar la columna 'telefono' de la tabla 'usuario'
--- Advertencia: Esto borrará todos los datos de teléfonos existentes.
-
-ALTER TABLE public.usuario 
-DROP COLUMN IF EXISTS telefono;
-
--- Recargar caché de esquema para que la API deje de esperar este campo
-NOTIFY pgrst, 'reload config';
